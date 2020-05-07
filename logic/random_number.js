a2ac3d68b5641eb54d0944e089fa9fafe996a90b
Neeraj Kumar <neeraj.kumar@appsimity.com>
2020-05-07 16:52:46 +0530

    done

--- logic/random_number.js
+++ logic/random_number.js
@@ -1,5 +1,5 @@
 generateRandomNumber = () => {
-    return Math.floor(1000 + Math.random() * 9000);
+    return Math.floor(100000 + Math.random() * 900000);
 }
 
 module.exports = generateRandomNumber;
